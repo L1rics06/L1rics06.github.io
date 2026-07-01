469c355e5b9e9c7aebe2e251c4cd0dfca33174ac
L1rics2006@gmail.com <l1rics2006@gmail.com>
2026-07-02 00:53:10 +0800

    Add markdown math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 import sitemap from "@astrojs/sitemap";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://l1rics06.github.io",
   integrations: [sitemap()],
   markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [[rehypeKatex, { strict: false }]],
+    }),
     shikiConfig: {
       themes: {
         light: "github-light",
